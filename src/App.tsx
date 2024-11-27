dfdbd52c56278b19ac95e4d4053e6f44683905ae
Klaudia <kbilan1992@gmail.com>
2024-11-27 21:25:17 +0100

    final look

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import "./styles/App.css";
 const App: React.FC = () => {
     return (
         <>
-            <div className="background">
-                <div className="scanlines"></div>
-                <div className="intro-wrap">
-                    <div className="noise"></div>
-                    <div className="noise noise-moving"></div>
-                    <div className="play" data-splitting>PYTANIE CZY WYZWANIE?</div>
-                    <div className="time">--:--</div>
-                    <div className="recordSpeed">00:00:00</div>
-                </div>
-            </div>
-
-            {/* Karta aplikacji */}
             <div className="App">
                 <Game/>
             </div>
+            <div className="area">
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+
         </>
     );
 };
